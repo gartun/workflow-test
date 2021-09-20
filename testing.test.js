5aa8babbf28292a8e3f63db6306c78df7234b907
gartun <gartun@users.noreply.github.com>
2021-09-20 09:39:36 +0000

    Prettified Code!

--- testing.test.js
+++ testing.test.js
@@ -3,7 +3,7 @@ const fn = require("./testing");
 describe("testing name function", () => {
   it("contains the name that passed to it", () => {
     const name = "muzaffer canan";
-    
+
     expect(fn(name)).toContain(name);
-  })
-})
+  });
+});
