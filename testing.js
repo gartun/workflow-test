e2f1e272a70ea118acf64fe172052dafed26c5ab
gartun <artungokhan96@gmail.com>
2021-09-20 14:25:31 +0300

    jest

--- testing.js
+++ testing.js
@@ -1,6 +1,6 @@
 const fn = (name) => {
   
-    return `Hello there my friend, how are you doing? your name is ${name.toUpperCase()}, such a nice name`;
+    return `Hello there my friend, how are you doing? your name is ${name}, such a nice name`;
 };
 
 module.exports = fn;
